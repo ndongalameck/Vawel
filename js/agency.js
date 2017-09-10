4c87033749adeef3047084a571f9ad6c68df5518
#M2 Labs <antonioyosica@gmail.com>
2017-09-10 02:24:57 +0100

    Add files via upload

--- js/agency.js
+++ js/agency.js
@@ -21,6 +21,6 @@ $('body').scrollspy({
 })
 
 // Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
+/*$('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
-});
+});*/
